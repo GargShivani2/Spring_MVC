ed1f3a38a90b687dedf0e051a551ecb2a8f89fb9
Shivani Garg <bgargsonu@gmail.com>
2019-08-23 18:10:08 +0530

    Create Excersize.java

---
+++ src/main/java/org/shivani/model/Excersize.java
@@ -0,0 +1,13 @@
+package com.shivani.model;
+public class Excersize {
+    private int minutes;
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+
+}
