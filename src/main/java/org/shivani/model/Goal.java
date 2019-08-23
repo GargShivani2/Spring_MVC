a6b05ad5eef44e85c53e59c8f8750fce93a65e6c
Shivani Garg <bgargsonu@gmail.com>
2019-08-23 18:10:34 +0530

    Add files via upload

---
+++ src/main/java/org/shivani/model/Goal.java
@@ -0,0 +1,14 @@
+package com.shivani.model;
+
+public class Goal {
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    private int minutes;
+
+}
